31b88fca3b63bef3d266642b5d518304337695c0
Farah Noor <293658554+farahnoorcodes@users.noreply.github.com>
2026-07-07 20:31:37 +0500

    4 day

---
+++ 004_day_variables.py
@@ -0,0 +1,18 @@
+#= is used to assign values to variables in Python. Here are some examples of variable assignments:
+name="farah"#it's mean farah is stored in the variable name
+#you can print too like this:
+print(name)#be careful for the quotes , dont use it while printing variables but use will assignning it to a variable
+print("name")# it will print name as a string
+lastname="noor"# string caan be any word number decimal value sentence in qoutes
+print(name+lastname)#you can join 2 string variable by using + operator
+print(2*name)# by using * operator  you multiple the string with the integer number you multiply
+#print(name*lastname)# but you can't multiply,subtract,divide the two strings it will give error
+#now integers:
+prize=45#can be any integer +ve or -ve #without quotes its a number or with it will become a string
+print(prize)
+#now decimal/ float
+cpa=3.9# can be any floating/ decimal value
+print(cpa)
+#for boolean vales
+bulb_on=True#can bbe false
+print(bulb_on)
