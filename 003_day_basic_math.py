4851ad1022e66d7db02f02eb5dc7249bd8b28b01
Farah Noor <293658554+farahnoorcodes@users.noreply.github.com>
2026-07-07 20:06:52 +0500

    basic math

---
+++ 003_day_basic_math.py
@@ -0,0 +1,27 @@
+print(6+7)#if you want to print the sum of two or more numbers you don't use quotes.just write the numbers and the operator between them.
+# otherwise it will print the numbers as a string and not as a sum of numbers.
+print("6+7")#it will print string
+#same for subtraction
+print(6-7)#it will print the difference of two numbers
+print("6-7")#it will print string
+#same for multiplication
+print(6*7)#it will print the product of two numbers
+print("6*7")#it will print string
+#same for division
+print(6/7)#it will print the quotient of two numbers
+print("6/7")#it will print string
+#// is used for floor division
+print(6//7)#it will print the quotient of two numbers without the decimal part, means it will give the closest integer value of the quotient.
+#% is used for modulus
+print(6%7)#it will print the remainder of the division of two numbers.
+#** is used for exponentiation
+print(6**7)#it will print the result of 6 raised to the power of 7.
+#< is used for less than comparison
+print(6<7)#it will print True if 6 is less than 7 otherwise it will print False.
+#> is used for greater than comparison  
+print(6>7)#it will print True if 6 is greater than 7 otherwise it will print False.
+#True and False are boolean values in python. They are used to represent the truth value of an expression. they are case sensitive and should be written with the first letter capitalized.they are keywords in python and cannot be used as variable names.we will learn more about boolean values,keywords,variables in the next chapter.
+#<= is used for less than or equal to comparison
+print(6<=7)#it will print True if 6 is less than or equal to 7 otherwise it will print False.
+#>= is used for greater than or equal to comparison
+print(6>=7)#it will print True if 6 is greater than or equal to 7 otherwise it will print False.
